a43df5a07b7fdcdce2c52b98d47a2a5290a7c1fa
Antony Valverde R <gatovalverde4@gmail.com>
2024-05-31 09:18:58 -0600

    Update link

--- src/App.js
+++ src/App.js
@@ -82,7 +82,7 @@ function App() {
           <button
             className="fab"
             onClick={() => window.location.href =
-              'https://1drv.ms/u/s!AtpHceJ44ymigvdsYMU4_15dqjIx2A?e=K6A10F'}
+              'https://1drv.ms/u/s!AtpHceJ44ymigvdt2apa0Q_QT_dRTg?e=vnjoeF'}
             style={{
               position: 'fixed',
               right: '30px',
